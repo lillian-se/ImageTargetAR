7486ab2e2c4b0da0764b9733be4438f14340609b
Lillian Brevik <77015337+lillian-se@users.noreply.github.com>
2022-11-19 16:32:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.146.0/build/three.module.js";
 // import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js";
 import { ARButton } from 'https://unpkg.com/three@0.133.0/examples/jsm/webxr/ARButton.js';
 
